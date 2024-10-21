bf397966f186a28bdb1c67956af9623d81d2cf65
K-AKABANE <kesf0419@gmail.com>
2024-10-22 01:57:44 +0900

    프로젝트 생성

---
+++ src/main/java/jobsignal/jobconnector/JobConnectorApplication.java
@@ -0,0 +1,13 @@
+package jobsignal.jobconnector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobConnectorApplication {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(JobConnectorApplication.class, args);
+    }
+    
+}
---
+++ src/test/java/jobsignal/jobconnector/JobConnectorApplicationTests.java
@@ -0,0 +1,13 @@
+package jobsignal.jobconnector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobConnectorApplicationTests {
+    
+    @Test
+    void contextLoads() {
+    }
+    
+}
